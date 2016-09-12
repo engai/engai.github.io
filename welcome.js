524d6973abb7fe30ce3a7348bc5f98fed889d33d
Ed Ngai <engai@ucsd.edu>
2016-09-12 00:26:40 -0700

    Added welcome messages to console

---
+++ welcome.js
@@ -0,0 +1,2 @@
+console.log("%cWelcome to edngai.com!",  "color: #5b7081; font-size: x-large");
+console.log("%cThis site is open sourced @ https://github.com/engai/engai.github.io",  "color: #5b7081;");
